71c9ef98f2b5840116c1d2a7c65e05b2dee7934e
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-12 11:12:48 +1100

    getting an api call

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,8 @@
 const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=42.3605,-71.0596&units=f';
+
+
+request({ url: url, json: true }, (error, response) => {
+    console.log(`${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degrees out `);
+})
