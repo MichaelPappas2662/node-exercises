a52de3804358d6549ab89018324bf49117824c0c
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-12 13:50:47 +1100

    refactoring

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,26 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
-const url = 'http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=sydney';
+const weatherUrl = 'http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=sydney';
 // pk.eyJ1IjoibWljaGFlbHBhcHBhczI2NjIiLCJhIjoiY2t1bmNmMmR4MGo2MjJvbGk5eXpxejBtNCJ9.7XioTRWNeL3DRktwRJ-ySA
-const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWljaGFlbHBhcHBhczI2NjIiLCJhIjoiY2t1bmNmMmR4MGo2MjJvbGk5eXpxejBtNCJ9.7XioTRWNeL3DRktwRJ-ySA"
+// const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWljaGFlbHBhcHBhczI2NjIiLCJhIjoiY2t1bmNmMmR4MGo2MjJvbGk5eXpxejBtNCJ9.7XioTRWNeL3DRktwRJ-ySA"
 
 
-request({ url: url, json: true }, (error, response) => {
-    console.log(`${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degrees out `);
+request({ url: weatherUrl, json: true }, (error, response) => {
+    const currentWeatherDescriptions = response.body.current.weather_descriptions[0];
+    const currentWeatherTemperature = response.body.current.temperature
+    
+    if (error) {
+        console.log('Unable to connect!');
+    } else if (response.body.error) {
+        console.log('Unable to find location');
+    }else {
+        console.log(`${currentWeatherDescriptions}.It is currently ${currentWeatherTemperature} degrees out `);
+    }
 })
 
-request({url: geocodeUrl, json:true }, (error, response) => {
-    console.log(`${response.body.features[0].center[0]} and ${response.body.features[0].center[1]}`);
+
+geocode('Sydney', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
 })
