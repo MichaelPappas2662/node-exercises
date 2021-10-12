17299ca823be3e31cf1eb021a0dec7d2a6efdbf4
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-12 12:09:40 +1100

    adding geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,14 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=42.3605,-71.0596&units=f';
+const url = 'http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=sydney';
+// pk.eyJ1IjoibWljaGFlbHBhcHBhczI2NjIiLCJhIjoiY2t1bmNmMmR4MGo2MjJvbGk5eXpxejBtNCJ9.7XioTRWNeL3DRktwRJ-ySA
+const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWljaGFlbHBhcHBhczI2NjIiLCJhIjoiY2t1bmNmMmR4MGo2MjJvbGk5eXpxejBtNCJ9.7XioTRWNeL3DRktwRJ-ySA"
 
 
 request({ url: url, json: true }, (error, response) => {
     console.log(`${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degrees out `);
+})
+
+request({url: geocodeUrl, json:true }, (error, response) => {
+    console.log(`${response.body.features[0].center[0]} and ${response.body.features[0].center[1]}`);
 })
