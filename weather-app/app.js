8856292155f943d6923de6c8262456e95611a2eb
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-12 17:27:31 +1100

    destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,22 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
-const forecas= require('./utils/forecast')
+const forecast= require('./utils/forecast')
 
-const weatherUrl = 'http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=sydney';
-// pk.eyJ1IjoibWljaGFlbHBhcHBhczI2NjIiLCJhIjoiY2t1bmNmMmR4MGo2MjJvbGk5eXpxejBtNCJ9.7XioTRWNeL3DRktwRJ-ySA
-// const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWljaGFlbHBhcHBhczI2NjIiLCJhIjoiY2t1bmNmMmR4MGo2MjJvbGk5eXpxejBtNCJ9.7XioTRWNeL3DRktwRJ-ySA"
+const address = process.argv[2];
 
-
-
-
-geocode('Sydney', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-})
+if(!address){
+    console.log('Please provide an address');
+}else {
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+        if (error){
+            return console.log(error);
+        }
+        
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(location);
+            console.log(forecastData);
+        });
+    });  
+}
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,13 +1,16 @@
-const forecast = (latitude, longitude,  callback) => {
-    const weatherUrl = `http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=${latitude},${longitude}`;
+const request = require('request');
 
-    request({ url: weatherUrl, json: true }, (error, response) => {
+
+const forecast = (latitude, longitude,  callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=01d9a447011c42178aca45028f9bacbc&query=${latitude},${longitude}`;
+
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         }else {
-            callback(undefined,  `${response.body.current.weather_descriptions[0]}. It is currently${response.body.current.temperature} degrees out.`
+            callback(undefined,  `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out.`
             )
         }
     })
