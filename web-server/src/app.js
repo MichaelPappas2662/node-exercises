6c655f0a618552ba5f91aac3a1fdb6b7ce5e35f9
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-15 13:34:09 +1100

    adding hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,39 @@
 const path = require('path')
 const express = require('express');
 
-
-
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define paths for Express config 
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to server
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Michael Pappas'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Michael Pappas'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('about', {
+        title: 'Help',
+        name: 'Michael Pappas'
+    })
+})
 
 
 
