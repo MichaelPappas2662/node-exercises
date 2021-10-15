3800ecb329a316f016a3958f2ab9a651bdae9488
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-15 12:58:13 +1100

    adding more pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,15 @@
+const path = require('path')
 const express = require('express');
 
-const app = express();
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-});
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath))
+
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
-})
+});
